5542f2c35d8dcdb3385361b8673daecfb6c2db9f
Arthur A. Selimov <am35a.piston@gmail.com>
2022-11-11 22:32:04 +0400

    fixed header layout

--- App/src/components/authorized/header/Header.svelte
+++ App/src/components/authorized/header/Header.svelte
@@ -3,14 +3,24 @@
 </script>
 
 <header>
-    header
+    <div>
+        Organizations
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+    </svg>
 </header>
 
 <style lang="sass">
     header
-        // grid-column: 1/2
-        // grid-row: 1/2
-        // display: grid
-        // align-self: start
-        // gap: var(--gap-sm)
+        display: grid
+        gap: var(--gap-xs)
+        grid-template-columns: 1fr max-content
+        padding: var(--padding-xs) var(--padding-sm)
+        align-self: center
+        background-color: var(--face-background-color)
+        color: var(--main-color-75)
+        font-size: var(--font-size-lg)
+        font-weight: 700
 </style>
