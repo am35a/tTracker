ab449f6f5dd7b26ae04dad5ad0a6e00e37cd063c
Arthur A. Selimov <am35a.piston@gmail.com>
2022-11-12 22:18:12 +0400

    first try

--- App/src/document/layout/Emploers.svelte
+++ App/src/document/layout/Emploers.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import Main from '../../components/authorized/Main.svelte'
+    // import Main from '$src/authorized/Main.svelte'
     import Header from '../../components/authorized/header/Header.svelte'
     import Section from '../../components/authorized/section/Section.svelte'
     import Footer from '../../components/authorized/footer/Footer.svelte'
--- App/vite.config.ts
+++ App/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import sveltePreprocess from 'svelte-preprocess'
+// import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,15 @@ export default defineConfig({
         svelte({
             preprocess: sveltePreprocess()
         }),
-    ]
+    ],
+    resolve: {
+        alias: {
+            $cmp: 'src/components'
+            // 'src': path.resolve(__dirname, 'src'),
+            // 'cmp': path.resolve(__dirname, './src/components'),
+            // src: path.resolve('src/components/')
+            // cmp: path.resolve('./src/components'),
+            // $msw: path.resolve("./src/msw")
+        }
+    }
 })
