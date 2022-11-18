2d92b17fa412e7bf9d91726904f61aca0adf4210
Arthur A. Selimov <am35a.piston@gmail.com>
2022-11-18 23:07:25 +0300

    first build cmp

--- App/src/components/authorized/section/card/Card.svelte
+++ App/src/components/authorized/section/card/Card.svelte
@@ -1,23 +1,61 @@
-<script lang="ts">
-
-</script>
-
-<div class="card">
+<div class="card color-1">
     <div class="aside">
-        as
+        <slot name="aside"/>
+    </div>
+    <div class="header">
+        <slot name="header"/>
     </div>
     <div class="body">
-        <slot></slot>
+        <slot name="body"/>
     </div>
     <div class="footer">
-        footer
+        <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M4 0h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z"/>
+            </svg>
+            <span>
+                <slot name="jobs"/>
+            </span>
+        </div>
     </div>
 </div>
 
 <style lang="sass">
     .card
-        background-color: var(--background-color-button)
+        display: grid
+        grid-template-columns: max-content 1fr
+        grid-template-rows: repeat(3, max-content)
+        gap: var(--gap-xs)
         border-radius: var(--border-radius-md)
         padding: var(--padding-xs)
         box-shadow: var(--box-shadow-sm)
+        &.color
+            &-1
+                background-color: var(--color-light-1)
+                .aside
+                    background-color: var(--face-background-color)
+        .aside
+            grid-column: 1/2
+            grid-row: 1/4
+            width: 16px
+            padding: var(--padding-xs)
+            border-radius: var(--border-radius-sm)
+        .header
+            // padding-top: var(--padding-xs)
+            font-size: var(--font-size-md)
+            font-weight: 700
+            color: var(--main-color-75)
+        .body
+            font-size: var(--font-size-sm)
+        .footer
+            display: grid
+            grid-template-columns: 1fr 1fr
+            // padding-bottom: var(--padding-xs)
+            > div
+                display: flex
+                gap: var(--gap-xs)
+                align-items: center
+                color: var(--main-color-75)
+                font-size: var(--font-size-xs)
+
 </style>
--- App/src/document/layout/Emploers.svelte
+++ App/src/document/layout/Emploers.svelte
@@ -12,11 +12,24 @@
     <Header/>
     <Section>
         <Calendar slot="calendar"/>
-        {#each Array(55) as _, i }
+        <!-- {#each Array(55) as _, i } -->
             <Card>
-                emploer card
+                <svelte:fragment slot="aside">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                        <path d="m.334 0 4.358 4.359h7.15v7.15l4.358 4.358V0H.334ZM.2 9.72l4.487-4.488v6.281h6.28L6.48 16H.2V9.72Z"/>
+                    </svg>
+                </svelte:fragment>
+                <svelte:fragment slot="header">
+                    AMD
+                </svelte:fragment>
+                <svelte:fragment slot="body">
+                    Here will be extra info or atension message.
+                </svelte:fragment>
+                <svelte:fragment slot="jobs">
+                    21
+                </svelte:fragment>
             </Card>
-        {/each}
+        <!-- {/each} -->
         <Filter slot="filter"/>
     </Section>
     <Footer/>
