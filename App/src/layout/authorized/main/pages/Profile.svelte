1d4d3d8907b7ff8d7f46c172ae2d8c8fbd97b62c
Arthur A. Selimov <am35a.piston@gmail.com>
2022-11-25 09:27:54 +0300

    start profile mockup

--- App/src/layout/authorized/main/pages/Profile.svelte
+++ App/src/layout/authorized/main/pages/Profile.svelte
@@ -1,7 +1,32 @@
 <script lang="ts">
+    // import Input from '$cmp/forms/Input.svelte'
+    import Button from '$cmp/buttons/Button.svelte'
     import Section from './section/Section.svelte'
+
+    // import { user } from '$str/store'
 </script>
 
 <Section>
-    Profile
+    <div>
+        <details>
+            <summary>
+                User data:
+            </summary>
+            Fist name
+            Second name
+            Registration date
+            <hr>
+            <Button>SAVE</Button>
+        </details>
+        <details>
+            <summary>
+                Privasy:
+            </summary>
+            Email
+            Password
+            Password
+            <Button>SAVE</Button>
+        </details>
+        <Button>SIGN OUT</Button>
+    </div>
 </Section>
